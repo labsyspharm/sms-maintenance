8e0b2c9ef447be7109854f18ff3daf85a36eaeca
Clemens Hug <clemens.hug@gmail.com>
2020-02-05 12:15:57 -0500

    Add compound mass calculation

--- id_mapping/04_compound_similarity.R
+++ id_mapping/04_compound_similarity.R
@@ -3,6 +3,7 @@ library(furrr)
 library(here)
 library(synapser)
 library(synExtra)
+library(lspcheminf)
 
 synLogin()
 syn <- synDownloader(here("tempdl"))
@@ -11,12 +12,18 @@ release <- "chembl_v25"
 dir_release <- here(release)
 syn_release <- synFindEntityId(release, "syn18457321")
 
-# Calculate similarity between all compounds -----------------------------------
+# Loading files ----------------------------------------------------------------
 ###############################################################################T
 
 all_compounds_fingerprints <- syn("syn20692501") %>%
   read_rds() %>%
   mutate(fn = map_chr(1:n(), ~tempfile(fileext = ".fps")))
+
+cmpds_canonical <- syn("syn20692514") %>%
+  read_csv()
+
+# Calculate similarity between all compounds -----------------------------------
+###############################################################################T
 
 all_compounds_fingerprints %>%
   # Adding a newline char at the end
@@ -40,6 +47,41 @@ similarity_df <- similarity_res %>%
 
 write_csv(similarity_df, file.path(dir_release, "all_compounds_similarity.csv.gz"))
 
+# Calculate compound mass -----------------------------------
+###############################################################################T
+
+# To establish identity between compounds, require that the topological FP
+# matches and one of the Morgan FPs and that their molecular mass is identical
+
+plan(multisession(workers = 4))
+cmpd_mass_raw <- cmpds_canonical %>%
+  distinct(inchi) %>%
+  drop_na(inchi) %>%
+  chunk_df(12) %>%
+  future_map(
+    ~molecular_mass(
+      set_names(.x$inchi)
+    ),
+    .progress = TRUE
+  ) %>%
+  bind_rows() %>%
+  distinct()
+
+cmpd_mass <- cmpd_mass_raw %>%
+  left_join(
+    cmpds_canonical %>%
+      select(source_name, id, inchi),
+    by = c("compound" = "inchi")
+  )
+
+cmpd_mass_map <- cmpd_mass %>%
+  distinct(source_name, id, mass)
+
+write_csv(
+  cmpd_mass_map,
+  file.path(dir_release, "compound_masses.csv.gz")
+)
+
 # Store to synapse -------------------------------------------------------------
 ###############################################################################T
 
@@ -56,7 +98,8 @@ syn_id_mapping <- Folder("id_mapping", parent = syn_release) %>%
   chuck("properties", "id")
 
 c(
-  file.path(dir_release, "all_compounds_similarity.csv.gz")
+  file.path(dir_release, "all_compounds_similarity.csv.gz"),
+  file.path(dir_release, "compound_masses.csv.gz")
 ) %>%
   synStoreMany(parentId = syn_id_mapping, activity = activity)
 
