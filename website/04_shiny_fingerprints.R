2cde68759425e89e6a97c2c5e001058611118875
Clemens Hug <clemens.hug@gmail.com>
2020-02-18 17:15:29 -0500

    Add fingerprints in morgancpp format

---
+++ website/04_shiny_fingerprints.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(here)
+library(fst)
+library(synapser)
+library(synExtra)
+library(morgancpp)
+
+synLogin()
+syn <- synDownloader(here("tempdl"), followLink = TRUE)
+
+# set directories, import files ------------------------------------------------
+###############################################################################T
+release <- "chembl_v25"
+dir_release <- here(release)
+syn_release <- synFindEntityId(release, "syn18457321")
+
+syn_tables <- "syn20981852"
+
+fingerprints <- syn("syn21042105") %>%
+  read_rds()
+
+# Fingerprints -----------------------------------------------------------------
+###############################################################################T
+
+fingerprint_tables <- fingerprints %>%
+  mutate(
+    data = map(
+      data,
+      ~.x %>%
+        arrange(lspci_id) %>%
+        filter(fp_name == "morgan_normal") %>%
+        select(lspci_id, fingerprint)
+    )
+  )
+
+activity <- Activity(
+  name = "Convert fingerprints to binary files used by morgancpp",
+  used = c(
+    "syn21042105"
+  ),
+  executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/website/04_shiny_fingerprints.R"
+)
+
+pwalk(
+  fingerprint_tables,
+  function(data, fp_name, ...) {
+    fps <- MorganFPS$new(data[["fingerprint"]])
+    fps$save_file(
+      file.path(dir_release, paste0("shiny_fingerprints_", fp_name, ".bin"))
+    )
+    syn_fp <- synPluck(syn_tables, fp_name)
+    syn_parent <- Folder("website", parent = syn_fp) %>%
+      synStore() %>%
+      chuck("properties", "id")
+    File(
+      file.path(dir_release, paste0("shiny_fingerprints_", fp_name, ".bin")),
+      parent = syn_parent,
+      name = "shiny_fingerprints.bin"
+    ) %>%
+      synStore(activity = activity)
+  }
+)
