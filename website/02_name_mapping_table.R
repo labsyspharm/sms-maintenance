27e9f314d8aa2c8529214f3e4701fcfa022ad73e
Clemens Hug <clemens.hug@gmail.com>
2020-04-07 10:07:32 -0400

    Fix name mapping tables for shiny

--- website/02_name_mapping_table.R
+++ website/02_name_mapping_table.R
@@ -21,6 +21,8 @@ canonical_table <- syn("syn20835543") %>%
 # Name mapping table -----------------------------------------------------------
 ###############################################################################T
 
+NAME_SOURCE_PREFERENCE <- c("pref_name", "alt_name", "chembl_id", "hms_id")
+
 gather_names <- function(d) {
   d %>%
     as.data.table() %>%
@@ -31,10 +33,34 @@ gather_names <- function(d) {
       id.vars = "lspci_id", variable.name = "source", value.name = "name", variable.factor = FALSE
     ) %>%
     {
-      .[, list(name = unlist(name)), by = list(lspci_id, source)][!is.na(name)]
+      .[
+        , list(name = unlist(name)), by = list(lspci_id, source)
+      ][
+        , source := factor(source, levels = NAME_SOURCE_PREFERENCE)
+      ][
+        !is.na(name)
+      ][
+        , name := if_else(str_length(name) > 1000, str_sub(str_split_fixed(name, "\\s", n = 2)[, 1], end = 1000), name)
+      ]
     } %>%
-    setkey(lspci_id, source) %>%
-    as_tibble()
+    setkey(lspci_id, source)
+}
+
+make_lspci_name_mapping <- function(d) {
+  d %>%
+    {
+      .[, head(.SD, 1), by = lspci_id][, list(lspci_id, name)]
+    } %>%
+    setkey(lspci_id)
+}
+
+make_name_lspci_mapping <- function(d) {
+  d %>%
+    # We want higher priority sources to come up first
+    setkey(source, lspci_id) %>%
+    {
+      .[, list(lspci_id, name)]
+    }
 }
 
 name_mapping <- canonical_table %>%
@@ -42,7 +68,9 @@ name_mapping <- canonical_table %>%
     data = map(
       data,
       gather_names
-    )
+    ),
+    name_lspci_id_map = map(data, make_name_lspci_mapping),
+    lspci_id_name_map = map(data, make_lspci_name_mapping)
   )
 
 activity <- Activity(
@@ -55,20 +83,28 @@ activity <- Activity(
 
 pwalk(
   name_mapping,
-  function(fp_name, data, ...) {
+  function(fp_name, data, name_lspci_id_map, lspci_id_name_map, ...) {
     write_csv(
       data,
-      file.path(dir_release, paste0("lspci_id_name_mapping_", fp_name, ".csv.gz"))
+      file.path(dir_release, paste0("lspci_id_name_map.csv.gz"))
+    )
+    write_fst(
+      name_lspci_id_map,
+      file.path(dir_release, paste0("name_lspci_id_map.fst"))
+    )
+    write_fst(
+      lspci_id_name_map,
+      file.path(dir_release, paste0("lspci_id_name_map.fst"))
     )
     syn_fp <- synPluck(syn_tables, fp_name)
     syn_parent <- Folder("website", parent = syn_fp) %>%
       synStore() %>%
       chuck("properties", "id")
-    File(
-      file.path(dir_release, paste0("lspci_id_name_mapping_", fp_name, ".csv.gz")),
-      parent = syn_parent,
-      name = "lspci_id_name_map.csv.gz"
+    c(
+      # file.path(dir_release, paste0("lspci_id_name_map.csv.gz")),
+      file.path(dir_release, paste0("name_lspci_id_map.fst"))
+      # file.path(dir_release, paste0("lspci_id_name_map.fst"))
     ) %>%
-      synStore(activity = activity)
+      synStoreMany(syn_parent, activity = activity)
   }
 )
