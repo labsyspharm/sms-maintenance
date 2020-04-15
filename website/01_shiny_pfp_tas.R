8a2f4aa36876678dcf2bf86bbb7ca9322f49a2d5
Clemens Hug <clemens.hug@gmail.com>
2020-04-15 12:01:49 -0400

    Add sources for TAS vector for website

--- website/01_shiny_pfp_tas.R
+++ website/01_shiny_pfp_tas.R
@@ -19,25 +19,32 @@ syn_tables <- "syn20981852"
 rscores <- syn("syn21590477") %>%
   read_rds()
 
-tas <- syn("syn20830939") %>%
+tas <- syn("syn21664452") %>%
   read_rds()
 
 # Phenotypic score and TAS tables ----------------------------------------------
 ###############################################################################T
 
-# Remove any compounds with less than 6 assays
+# Remove any compounds with less than 2 assays
 rscores_selected <- rscores %>%
   mutate(
     data = map(
       data,
       function(d) {
-        res <- d %>%
+        d %>%
+          mutate_at(vars(lspci_id, assay_id), as.integer) %>%
           as.data.table() %>%
-          .[is.finite(rscore_tr), .(lspci_id, assay_id, rscore_tr)] %>%
-          .[, if (.N >= 6) .SD, by = lspci_id]
-        # Set index on the assay and lspci id for efficient joining
-        setkey(res, assay_id, lspci_id)
-        res
+          {
+            .[
+              is.finite(rscore_tr)
+            ][
+              , .(lspci_id, assay_id, rscore_tr)
+            ][
+              , if (.N >= 2) .SD, by = lspci_id
+            ]
+          } %>%
+          # Set index on the assay and lspci id for efficient joining
+          setkey(assay_id, lspci_id)
       }
     )
   )
@@ -47,7 +54,7 @@ activity <- Activity(
   used = c(
     "syn21590477"
   ),
-  executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/data_processing/11_prepare_website_files.R"
+  executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/website/01_shiny_pfp_tas.R"
 )
 
 pwalk(
@@ -55,42 +62,41 @@ pwalk(
   function(fp_name, data, ...) {
     write_fst(
       data,
-      file.path(dir_release, paste0("phenotypic_rscore_", fp_name, ".fst"))
+      file.path(dir_release, paste0("shiny_phenotypic_rscore_", fp_name, ".fst"))
     )
     syn_fp <- synPluck(syn_tables, fp_name)
     syn_parent <- Folder("website", parent = syn_fp) %>%
       synStore() %>%
       chuck("properties", "id")
     File(
-      file.path(dir_release, paste0("phenotypic_rscore_", fp_name, ".fst")),
+      file.path(dir_release, paste0("shiny_phenotypic_rscore_", fp_name, ".fst")),
       parent = syn_parent,
-      name = "phenotypic_rscore.fst"
+      name = "shiny_phenotypic_rscore.fst"
     ) %>%
       synStore(activity = activity)
   }
 )
 
 
-# Remove any compounds with less than 6 assays
+# Remove any compounds with less than 2 assays
 tas_selected <- tas %>%
   mutate(
     data = map(
       data,
       function(d) {
-        res <- d %>%
+        d %>%
+          mutate_at(vars(lspci_id, entrez_gene_id, tas), as.integer) %>%
+          select(
+            lspci_id, gene_id = entrez_gene_id, tas, source, measurement, references
+          ) %>%
           as.data.table() %>%
-          .[
-            ,
-            .(lspci_id, entrez_gene_id, tas)
-          ] %>%
-          .[
-            ,
-            if (.N >= 6) .SD,
-            by = lspci_id
-          ]
-        # Set index on the entrez_gene_id and lspci id for efficient joining
-        setkey(res, entrez_gene_id, lspci_id)
-        res
+          {
+            .[
+              , if (.N >= 2) .SD, by = lspci_id
+            ]
+          } %>%
+          # Set index on the entrez_gene_id and lspci id for efficient joining
+          setkey(gene_id, lspci_id)
       }
     )
   )
@@ -98,9 +104,9 @@ tas_selected <- tas %>%
 activity <- Activity(
   name = "Wrangle TAS scores for website",
   used = c(
-    "syn20830939"
+    "syn21664452"
   ),
-  executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/data_processing/11_prepare_website_files.R"
+  executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/website/01_shiny_pfp_tas.R"
 )
 
 pwalk(
@@ -108,16 +114,16 @@ pwalk(
   function(fp_name, data, ...) {
     write_fst(
       data,
-      file.path(dir_release, paste0("tas_score_", fp_name, ".fst"))
+      file.path(dir_release, paste0("shiny_tas_", fp_name, ".fst"))
     )
     syn_fp <- synPluck(syn_tables, fp_name)
     syn_parent <- Folder("website", parent = syn_fp) %>%
       synStore() %>%
       chuck("properties", "id")
     File(
-      file.path(dir_release, paste0("tas_score_", fp_name, ".fst")),
+      file.path(dir_release, paste0("shiny_tas_", fp_name, ".fst")),
       parent = syn_parent,
-      name = "tas_score.fst"
+      name = "shiny_tas.fst"
     ) %>%
       synStore(activity = activity)
   }
