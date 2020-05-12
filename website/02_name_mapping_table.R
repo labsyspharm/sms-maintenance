826ce1b0eba16bae90d128708882091580d07cae
Clemens Hug <clemens.hug@gmail.com>
2020-05-12 16:46:24 -0400

    Re-wrangle name mapping tables for SMS website including sources

--- website/02_name_mapping_table.R
+++ website/02_name_mapping_table.R
@@ -16,38 +16,28 @@ syn_release <- synFindEntityId(release, "syn18457321")
 
 syn_tables <- "syn20981852"
 
-canonical_table <- syn("syn20835543") %>%
+eq_classes <- syn("syn20830516") %>%
   read_rds()
 
-target_table <- syn("syn20693721") %>%
-  read_csv()
+all_names <- syn("syn22035396") %>%
+  read_rds()
 
 # Compound name mapping table --------------------------------------------------
 ###############################################################################T
 
-NAME_SOURCE_PREFERENCE <- c("pref_name", "alt_name", "chembl_id", "hms_id")
-
-gather_names <- function(d) {
-  d %>%
-    as.data.table() %>%
-    {
-      .[, list(lspci_id, pref_name, alt_name = alt_names, chembl_id, hms_id)]
-    } %>%
-    melt(
-      id.vars = "lspci_id", variable.name = "source", value.name = "name", variable.factor = FALSE
-    ) %>%
-    {
-      .[
-        , list(name = unlist(name)), by = list(lspci_id, source)
-      ][
-        , source := factor(source, levels = NAME_SOURCE_PREFERENCE)
-      ][
-        !is.na(name)
-      ][
-        , name := if_else(str_length(name) > 1000, str_sub(str_split_fixed(name, "\\s", n = 2)[, 1], end = 1000), name)
-      ]
-    } %>%
-    setkey(lspci_id, source)
+gather_names <- function(names, ids, ...) {
+  list(
+    names,
+    ids %>%
+      transmute(
+        lspci_id = eq_class,
+        name = id,
+        source = as.factor(paste0(source, "_id")),
+        source_collapsed = as.factor("secondary")
+      )
+  ) %>%
+    rbindlist(use.names = TRUE) %>%
+    setkey(lspci_id, source_collapsed)
 }
 
 make_lspci_name_mapping <- function(d) {
@@ -55,46 +45,47 @@ make_lspci_name_mapping <- function(d) {
     {
       .[, head(.SD, 1), by = lspci_id][, list(lspci_id, name)]
     } %>%
-    setkey(lspci_id)
+    unique()
 }
 
-make_name_lspci_mapping <- function(d) {
-  d %>%
-    # We want higher priority sources to come up first
-    setkey(source, lspci_id) %>%
-    {
-      .[, list(lspci_id, name)]
-    }
-}
-
-name_mapping <- canonical_table %>%
-  mutate(
-    data = map(
-      data,
+name_mapping <- all_names %>%
+  rename(names = data) %>%
+  inner_join(
+    rename(eq_classes, ids = data)
+  ) %>%
+  transmute(
+    fp_name, fp_type,
+    data = pmap(
+      .,
       gather_names
-    ),
-    name_lspci_id_map = map(data, make_name_lspci_mapping),
-    lspci_id_name_map = map(data, make_lspci_name_mapping)
+    )
+  ) %>%
+  mutate(
+    lspci_id_name_map = map(
+      data,
+      make_lspci_name_mapping
+    )
   )
 
 activity <- Activity(
   name = "Wrangle name mapping table",
   used = c(
-    "syn20835543"
+    "syn20830516",
+    "syn22035396"
   ),
   executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/website/02_name_mapping_table.R"
 )
 
 pwalk(
   name_mapping,
-  function(fp_name, data, name_lspci_id_map, lspci_id_name_map, ...) {
+  function(fp_name, data, lspci_id_name_map, ...) {
     write_csv(
       data,
-      file.path(dir_release, paste0("lspci_id_name_map.csv.gz"))
+      file.path(dir_release, paste0("all_names_lspci_id_map.csv.gz"))
     )
     write_fst(
-      name_lspci_id_map,
-      file.path(dir_release, paste0("name_lspci_id_map.fst"))
+      data,
+      file.path(dir_release, paste0("all_names_lspci_id_map.fst"))
     )
     write_fst(
       lspci_id_name_map,
@@ -105,8 +96,8 @@ pwalk(
       synStore() %>%
       chuck("properties", "id")
     c(
-      file.path(dir_release, paste0("lspci_id_name_map.csv.gz")),
-      file.path(dir_release, paste0("name_lspci_id_map.fst")),
+      file.path(dir_release, paste0("all_names_lspci_id_map.csv.gz")),
+      file.path(dir_release, paste0("all_names_lspci_id_map.fst")),
       file.path(dir_release, paste0("lspci_id_name_map.fst"))
     ) %>%
       synStoreMany(syn_parent, activity = activity)
