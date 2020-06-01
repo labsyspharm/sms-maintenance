b7e0d677ed45827def0cf04915cf1356969bd4e5
Clemens Hug <clemens.hug@gmail.com>
2020-06-01 14:12:48 -0400

    Export new TAS vectors to INDRA

--- db_upload/02_indra_export.R
+++ db_upload/02_indra_export.R
@@ -17,18 +17,43 @@ db_version <- "morgan_normal"
 # read tables ------------------------------------------------------------------
 ###############################################################################T
 
-tas_annotated <- syn("syn20830941") %>%
+name_map <- syn("syn22035396") %>%
+  read_rds() %>%
+  filter(fp_name == db_version) %>%
+  chuck("data", 1)
+
+tas_annotated <- syn("syn21664452") %>%
   read_rds()
+
+id_map <- syn("syn20830516") %>%
+  read_rds() %>%
+  filter(fp_name == db_version) %>%
+  chuck("data", 1) %>%
+  transmute(
+    lspci_id = eq_class,
+    id_source = if_else(str_starts(.[["id"]], fixed("CHEMBL")), "chembl", "hmsl"),
+    compound_id = id
+  ) %>%
+  arrange(lspci_id)
 
 tas_table <- tas_annotated %>%
   filter(fp_name == db_version) %>%
   chuck("data", 1) %>%
-  filter(compound_id_source == "chembl") %>%
-  select(lspci_id, chembl_id = compound_id, entrez_gene_id, entrez_gene_symbol = entrez_symbol, tas)
+  drop_na(lspci_id, entrez_gene_id, tas)
 
 write_csv(
   tas_table,
-  file.path(dir_release, "indra_tas.csv")
+  file.path(dir_release, "indra_tas.csv.gz")
+)
+
+write_csv(
+  id_map,
+  file.path(dir_release, "indra_id_map.csv.gz")
+)
+
+write_csv(
+  name_map,
+  file.path(dir_release, "indra_name_map.csv.gz")
 )
 
 # Store to synapse -------------------------------------------------------------
@@ -37,7 +62,9 @@ write_csv(
 export_tas_indra <- Activity(
   name = "Export TAS data to INDRA",
   used = c(
-    "syn20830941"
+    "syn22035396",
+    "syn21664452",
+    "syn20830516"
   ),
   executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/db_upload/02_indra_export.R"
 )
@@ -47,7 +74,9 @@ syn_export <- Folder("export", parent = syn_release) %>%
   chuck("properties", "id")
 
 c(
-  file.path(dir_release, "indra_tas.csv")
+  file.path(dir_release, "indra_tas.csv.gz"),
+  file.path(dir_release, "indra_id_map.csv.gz"),
+  file.path(dir_release, "indra_name_map.csv.gz")
 ) %>%
   synStoreMany(parentId = syn_export, activity = export_tas_indra)
 
