568c4bd7119569efb5beb91781c1785ebd4307ea
Clemens Hug <clemens.hug@gmail.com>
2021-01-19 22:36:15 -0500

    Make fingerprint table for ChEMBL v27

--- id_mapping/07_fingerprint_table.R
+++ id_mapping/07_fingerprint_table.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(here)
 library(synapser)
 library(synExtra)
+library(data.table)
 
 synLogin()
 syn <- synDownloader(here("tempdl"))
@@ -11,45 +12,51 @@ syn <- synDownloader(here("tempdl"))
 
 # Set directories, import files ------------------------------------------------
 ###############################################################################T
-release <- "chembl_v25"
+release <- "chembl_v27"
 dir_release <- here(release)
 syn_release <- synFindEntityId(release, "syn18457321")
+
+
+inputs <- list(
+  all_fp = synPluck(syn_release, "fingerprints", "all_compounds_fingerprints.csv.gz"),
+  canonical_members = synPluck(syn_release, "compounds_processed", "lspci_id_canonical_members.csv.gz")
+)
+
+input_data <- inputs %>%
+  map(syn) %>%
+  map(
+    function(x)
+      list(
+        `.csv` = partial(fread, colClasses = c(inchi_id = "integer")),
+        `.tsv` = fread,
+        `.rds` = read_rds
+      ) %>%
+      magrittr::extract2(which(str_detect(x, fixed(names(.))))) %>%
+      {.(x)}
+  )
+
 
 # Prepare fingerprint database -------------------------------------------------
 ###############################################################################T
 
-
-all_fp <- syn("syn20692501") %>%
-  read_rds()
-
-canonical_compounds <- syn("syn20835543") %>%
-  read_rds()
-
-all_fp_flat <- all_fp %>%
-  unnest(data)
-
-canonical_fp <- canonical_compounds %>%
-  mutate(
-    data = map(
-      data,
-      ~.x %>%
-        select(lspci_id, chembl_id, hms_id) %>%
-        gather("source", "id", hms_id, chembl_id) %>%
-        # Prefer Chembl if present
-        mutate(source = factor(source, levels = c("chembl_id", "hms_id"))) %>%
-        group_by(lspci_id) %>%
-        arrange(source, .by_group = TRUE) %>%
-        slice(1) %>%
-        ungroup() %>%
-        left_join(all_fp_flat, by = "id") %>%
-        select(lspci_id, fp_name, fp_type, fingerprint)
-    )
+fingerprint_table <- input_data[["canonical_members"]][
+  ,
+  .(
+    lspci_id,
+    inchi_id
   )
+] %>%
+  merge(
+    input_data[["all_fp"]],
+    all = FALSE,
+    by = "inchi_id"
+  ) %>%
+  setkey(lspci_id, fp_name)
 
-write_rds(
-  canonical_fp,
-  file.path(dir_release, "canonical_fingerprints.rds"),
-  compress = "gz"
+
+fwrite(
+  fingerprint_table,
+  file.path(dir_release, "lspci_id_fingerprints.csv.gz")
 )
 
 # Store to synapse -------------------------------------------------------------
@@ -57,19 +64,13 @@ write_rds(
 
 fp_table_activity <- Activity(
   name = "Create table of molecular fingerprints",
-  used = c(
-    "syn20692501",
-    "syn20835543"
-  ),
+  used = unname(inputs),
   executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/id_mapping/07_fingerprint_table.R"
 )
 
-fp_folder <- Folder("fingerprints", syn_release) %>%
-  synStore() %>%
-  chuck("properties", "id")
+fp_folder <- synMkdir(syn_release, "compounds_processed")
 
 c(
-  file.path(dir_release, "canonical_fingerprints.rds")
+  file.path(dir_release, "lspci_id_fingerprints.csv.gz")
 ) %>%
   synStoreMany(parent = fp_folder, activity = fp_table_activity)
-
