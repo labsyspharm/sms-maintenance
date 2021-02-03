37b302961197671f3abdc0abaa66fd29f4e72ce9
Clemens Hug <clemens.hug@gmail.com>
2021-02-03 11:00:37 -0500

    Aggregate data for ChEMBL v27

--- data_processing/02_aggregate_drug_affinities.R
+++ data_processing/02_aggregate_drug_affinities.R
@@ -21,13 +21,12 @@ inputs <- list(
   chembl_ref_info = c("raw_data", "chembl", "chembl_ref_info_raw.csv.gz"),
   chembl_biochemical = c("raw_data", "chembl", "chembl_biochemical_raw.csv.gz"),
   chembl_phenotypic = c("raw_data", "chembl", "chembl_phenotypic_raw.csv.gz"),
-  chembl_biochemical = c("raw_data", "chembl", "chembl_biochemical_raw.csv.gz")
+  chembl_biochemical = c("raw_data", "chembl", "chembl_biochemical_raw.csv.gz"),
+  target_dictionary = c("id_mapping", "target_dictionary_wide.csv.gz"),
+  inhouse_dose_response = c("raw_data", "hmsl", "hmsl_doseresponse.csv.gz"),
+  inhouse_single_dose = c("raw_data", "hmsl", "hmsl_singledose.csv.gz")
 ) %>%
-  map(~exec(synPluck, !!!c(syn_release, .x))) %>%
-  c(
-    inhouse_dose_response = "syn20692433",
-    inhouse_single_dose = "syn20692432"
-  )
+  map(~exec(synPluck, !!!c(syn_release, .x)))
 
 input_data <- inputs %>%
   map(syn) %>%
@@ -81,7 +80,7 @@ chembl_ref_info_best <- copy(input_data[["chembl_ref_info"]])[
 biochem_neat <- copy(input_data[["chembl_biochemical"]]) %>%
   mutate(standard_value = as.numeric(standard_value)) %>%
   rename(pref_name_target = pref_name) %>%
-  filter(tax_id == 9606, !is.na(entrez_gene_id)) %>%
+  filter(!is.na(entrez_gene_id) | !is.na(symbol)) %>%
   inner_join(
    copy(input_data[["lspci_id_vendor_id_map"]])[
      source == "chembl"
@@ -90,11 +89,10 @@ biochem_neat <- copy(input_data[["chembl_biochemical"]]) %>%
      source := NULL
    ],
    by = c("chembl_id_compound" = "vendor_id")
-  )
+  ) %>%
+  drop_na(lspci_id, entrez_gene_id)
 
 dose_response_inhouse_neat <- copy(input_data[["inhouse_dose_response"]]) %>%
-  mutate(hms_id = paste0("HMSL", hms_id)) %>%
-  rename(entrez_gene_id = gene_id, hmsl_id = hms_id) %>%
   inner_join(
     copy(input_data[["lspci_id_vendor_id_map"]])[
       source == "hmsl"
@@ -103,7 +101,8 @@ dose_response_inhouse_neat <- copy(input_data[["inhouse_dose_response"]]) %>%
       source := NULL
     ],
     by = c("hmsl_id" = "vendor_id")
-  )
+  ) %>%
+  drop_na(lspci_id, entrez_gene_id)
 
 pheno_data_neat <- copy(input_data[["chembl_phenotypic"]]) %>%
   mutate(standard_value = as.numeric(standard_value)) %>%
@@ -115,7 +114,8 @@ pheno_data_neat <- copy(input_data[["chembl_phenotypic"]]) %>%
       source := NULL
     ],
     by = c("chembl_id_compound" = "vendor_id")
-  )
+  ) %>%
+  drop_na(lspci_id, assay_id)
 
 # Aggregate dose-response data -------------------------------------------------
 ###############################################################################T
@@ -146,36 +146,25 @@ biochem_rowbind <- biochem_neat %>%
     lspci_id,
     value, value_unit = standard_units, value_type = standard_type,
     value_relation = standard_relation, description_assay = description,
-    uniprot_id, entrez_gene_id,
+    uniprot_id, entrez_gene_id, symbol,
     references, file_url
   )
 
-doseresponse_inhouse_rowbind <- input_data[["inhouse_dose_response"]] %>%
+doseresponse_inhouse_rowbind <- dose_response_inhouse_neat %>%
   mutate(
-    hms_id = paste0("HMSL", hms_id)
-  ) %>%
-  inner_join(
-    input_data[["lspci_id_vendor_id_map"]] %>%
-      filter(source == "hmsl") %>%
-      select(lspci_id, vendor_id),
-    by = c("hms_id" = "vendor_id")
-  ) %>%
-  mutate(
-    references = map(
-      synapse_id,
+    references = map2(
+      reference_type, reference_id,
       ~data.table(
-        reference_id = .x,
-        reference_type = factor("synapse_id", levels = REFERENCE_PRIORITY)
+        reference_id = .y,
+        reference_type = factor(.x, levels = REFERENCE_PRIORITY)
       )
-    ),
-    reference_id = synapse_id,
-    reference_type = "synapse_id"
+    )
   ) %>%
   select(
     lspci_id,
     value, value_unit, value_type,
     value_relation, description_assay = description,
-    uniprot_id, entrez_gene_id = gene_id,
+    uniprot_id, entrez_gene_id, symbol,
     references, file_url
   )
 
@@ -183,10 +172,6 @@ biochem_complete <- bind_rows(
   biochem_rowbind,
   doseresponse_inhouse_rowbind
 ) %>%
-  # Call to distinct important, since some assays can be recorded multiple times
-  # for the same eq_class now, when multiple forms of the same drug where mapped
-  # to the same eq_class and an assay was stored in the db for all forms
-  distinct() %>%
   # Remap obsolete entrez_ids
   # 645840 -> 114112
   # 348738 -> 6241
@@ -194,7 +179,11 @@ biochem_complete <- bind_rows(
     entrez_gene_id = recode(
       entrez_gene_id, `645840` = 114112L, `348738` = 6241L
     )
-  )
+  ) %>%
+  # Call to distinct important, since some assays can be recorded multiple times
+  # for the same eq_class now, when multiple forms of the same drug where mapped
+  # to the same eq_class and an assay was stored in the db for all forms
+  distinct()
 
 qsave(
   biochem_complete,
@@ -224,14 +213,25 @@ calculate_q1 <- function(data) {
             } %>%
             paste(collapse = "|")
         ),
-        by = .(lspci_id, entrez_gene_id)
+        by = .(lspci_id, aggregate_column, aggregate_value)
       ]
     }
 }
 
 biochem_complete_q1 <- biochem_complete %>%
-  setkey(lspci_id, entrez_gene_id) %>%
-  head(n = 10000) %>%
+  mutate(
+    aggregate_column = if_else(
+      !is.na(entrez_gene_id) | is.na(symbol) | symbol == "-",
+      "entrez_gene_id",
+      "symbol"
+    ),
+    aggregate_value = if_else(
+      aggregate_column == "entrez_gene_id",
+      as.character(entrez_gene_id),
+      symbol
+    )
+  ) %>%
+  setkey(lspci_id, aggregate_column, aggregate_value) %>%
   calculate_q1()
 
 fwrite(
@@ -245,206 +245,89 @@ fwrite(
 
 # Also calculate Q1 values for kinomescan data from HMS LINCS for which no
 # complete dose response curve is available
-hmsl_kinomescan <- syn("syn20692432") %>%
-  read_csv(col_types = "cccdcdcc") %>%
-  # Some of the HMSL IDs don't start with "HMSL", fix that
-  mutate(hms_id = if_else(str_starts(hms_id, "HMSL"), hms_id, paste0("HMSL", hms_id)))
 
-# check how common the situation is where a single gene was tested in different
-# variants (mutants, post-translational modification, etc.)
-hmsl_kinomescan %>%
-  count(hms_id, gene_symbol, cmpd_conc_nM) %>%
-  count(n)
-# # A tibble: 21 x 2
-# n    nn
-# <int> <int>
-#   1     1 60448
-# 2     2  4467
-# 3     3   283
-# 4     4   250
-# 5     5    32
-# 6     6    57
-# 7     7    56
-# 8     8    88
-# 9     9   122
-# 10    10   161
-# # … with 11 more rows
-# It's very common, so we have to decide how to aggregate info for every
-# target/compound combo.
-# I think it makes sense to try to filter out mutant data, but keep post-translational
-# modified targets (especially phosphorylation), because those are likely to be
-# broadly relevant. if somebody is interested in specific mutations they will
-# have to query the Kinomescan directly and shoulnd't rely on TAS.
-
-hmsl_kinomescan_cleaned <- hmsl_kinomescan %>%
-  filter(
-    # Filter mutant annotation e.g. S154A
-    !grepl("[^A-Za-z]+[A-Z][0-9]+([A-Z]|del|Del)[^A-Za-z]", description),
-    !grepl("domain", description),
-    !grepl("Dom", description),
-    !grepl("inhibited", description)
-  )
-
-hmsl_kinomescan_mapped <- all_cmpds_eq_classes %>%
-  mutate(
-    data = map(
-      data,
-      ~hmsl_kinomescan_cleaned %>%
-        genebabel::join_hgnc(
-          "gene_symbol",
-          c("symbol", "alias_symbol", "prev_symbol"),
-          c("entrez_id", "name", "uniprot_ids")
-        ) %>%
-        # I checked, no gene_symbol maps to multiple uniprot, so this is safe
-        mutate(
-          uniprot_id = as.character(uniprot_ids),
-          # Either synapse ID or HMSL ID
-          reference_type = if_else(str_starts(source_assay_id, fixed("syn")), "synapse", "hms_lincs")
-        ) %>%
-        select(-uniprot_ids) %>%
-        left_join(
-          .x %>%
-            select(id, eq_class),
-          by = c("hms_id" = "id")
-        ) %>%
-        rename(
-          entrez_gene_id = entrez_id,
-          pref_name_cmpd = pref_name,
-          pref_name_target = name,
-          lspci_id = eq_class,
-          reference_id = source_assay_id,
-          file_url = url
-        ) %>%
-        drop_na(entrez_gene_id) %>%
-        # Remap obsolete entrez_ids
-        # 645840 -> 114112
-        # 348738 -> 6241
-        mutate(entrez_gene_id = recode(as.integer(entrez_gene_id), `645840` = 114112L, `348738` = 6241L))
-    )
-  )
-
-write_rds(
-  hmsl_kinomescan_mapped,
-  file.path(dir_release, "biochemicaldata_single_dose_inhouse.rds"),
-  compress = "gz"
-)
-
-hmsl_kinomescan_q1 <- hmsl_kinomescan_mapped %>%
-  mutate(
-    data = map(
-      data,
-      ~.x %>%
-        as.data.table() %>%
-        {
-          .[
-            ,
-            reference_type := recode(
+hmsl_kinomescan_q1_func <- function(data) {
+    n_groups <- uniqueN(data, by = c("lspci_id", "entrez_gene_id", "cmpd_conc_nM"))
+    pb <- txtProgressBar(min = 1, max = n_groups, style = 3)
+    on.exit(close(pb))
+    data %>%
+      as.data.table() %>% {
+        .[
+          ,
+          .(
+            percent_control_Q1 = {
+              setTxtProgressBar(pb, .GRP)
+              round(quantile(percent_control, 0.25, names = FALSE), 2)
+            },
+            n_measurement = .N,
+            references = paste(
               reference_type,
-              pubmed_id = "pubmed", patent_id = "patent", chembl_id = "chembl", synapse_id = "synapse"
-            )
-          ][
-            ,
-            .(
-              percent_control_Q1 = quantile(percent_control, 0.25, names = FALSE),
-              n_measurement = .N,
-              references = .SD[, .(reference_type, reference_id)] %>%
-                unique() %>%
-                with(
-                  paste(
-                    reference_type,
-                    reference_id,
-                    sep = ":",
-                    collapse = "|"
-                  )
-                )
-            ),
-            by = .(lspci_id, entrez_gene_id, cmpd_conc_nM)
-          ]
-        } %>%
-        as_tibble()
-    )
-  )
+              reference_id,
+              sep = ":"
+            ) %>%
+              unique() %>%
+              paste(collapse = "|")
+          ),
+          by = .(lspci_id, entrez_gene_id, cmpd_conc_nM)
+        ]
+      }
+  }
 
+hmsl_kinomescan_q1 <- input_data[["inhouse_single_dose"]] %>%
+  distinct(
+    lspci_id, entrez_gene_id, cmpd_conc_nM,
+    percent_control, reference_type, reference_id
+  ) %>%
+  hmsl_kinomescan_q1_func()
 
-write_rds(
+fwrite(
   hmsl_kinomescan_q1,
-  file.path(dir_release, "biochemicaldata_single_dose_inhouse_Q1.rds"),
-  compress = "gz"
+  file.path(dir_release, "inhouse_single_dose_data_complete_q1.csv.gz")
 )
 
 # Aggregate phenotypic data ----------------------------------------------------
 ###############################################################################T
 
 pheno_data_formatted <- pheno_data_neat %>%
-  mutate(
-    data = map(
-      data,
-      ~.x %>%
-        left_join(
-          select(chembl_ref_info_best, chembl_id_doc, reference_type, reference_id),
-          by = "chembl_id_doc"
-        ) %>%
-        mutate(
-          reference_id = if_else(is.na(reference_id), chembl_id_doc, reference_id),
-          reference_type = if_else(is.na(reference_type), "chembl_id", as.character(reference_type)),
-          file_url = paste0("https://www.ebi.ac.uk/chembl/document_report_card/", chembl_id_doc)
-        ) %>%
-        select(
-          lspci_id, assay_id,
-          value = standard_value, value_unit = standard_units, value_type = standard_type,
-          value_relation = standard_relation, description_assay = description,
-          reference_id, reference_type, file_url
-        )
-    )
+  inner_join(
+    chembl_ref_info_best,
+    by = "chembl_id_doc"
+  ) %>%
+  distinct(
+    lspci_id, assay_id,
+    value = standard_value, value_unit = standard_units, value_type = standard_type,
+    value_relation = standard_relation, description_assay = description,
+    references
   )
 
-write_rds(
+qsave(
   pheno_data_formatted,
-  file.path(dir_release, "pheno_data.rds"),
-  compress = "gz"
+  file.path(dir_release, "pheno_data.qs"),
+  preset = "fast"
 )
 
 pheno_data_q1 <- pheno_data_formatted %>%
-  mutate(
-    data = map(
-      data,
-      ~.x %>%
-        as.data.table() %>%
-        {
-          .[
-            ,
-            reference_type := recode(
-              reference_type,
-              pubmed_id = "pubmed", patent_id = "patent", chembl_id = "chembl", synapse_id = "synapse"
-            )
-            ][
-            ,
-            .(
-              standard_value_Q1 = quantile(value, 0.25, names = FALSE),
-              log10_value_Q1 = quantile(log10(value), 0.25, names = FALSE),
-              n_measurement = .N,
-              references = .SD[, .(reference_type, reference_id)] %>%
-                unique() %>%
-                with(
-                  paste(
-                    reference_type,
-                    reference_id,
-                    sep = ":",
-                    collapse = "|"
-                  )
-                )
-            ),
-            by = .(lspci_id, assay_id)
-          ]
-        } %>%
-        as_tibble()
-    )
-  )
+  setDT() %>%
+  {
+    .[
+      ,
+      .(
+        value_Q1 = quantile(value, 0.25, names = FALSE),
+        n_measurement = .N,
+        references = references %>%
+          rbindlist(use.names = TRUE) %>%
+          unique() %>% {
+            paste(.[["reference_type"]], .[["reference_id"]], sep = ":")
+          } %>%
+          paste(collapse = "|")
+      ),
+      by = .(lspci_id, assay_id)
+    ]
+  }
 
-write_rds(
+fwrite(
   pheno_data_q1,
-  file.path(dir_release, "pheno_data_Q1.rds"),
-  compress = "gz"
+  file.path(dir_release, "pheno_data_q1.csv.gz")
 )
 
 # Store to synapse -------------------------------------------------------------
@@ -452,27 +335,15 @@ write_rds(
 
 aggregation_activity <- Activity(
   name = "Aggregate affinity data",
-  used = c(
-    "syn20692432",
-    "syn20692433",
-    "syn20693825",
-    "syn20693827",
-    "syn20830516",
-    "syn21652213"
-  ),
+  used = unname(inputs),
   executed = "https://github.com/clemenshug/small-molecule-suite-maintenance/blob/master/data_processing/02_aggregate_drug_affinities.R"
 )
 
-syn_aggregate <- Folder("aggregate_data", parent = syn_release) %>%
-  synStore() %>%
-  chuck("properties", "id")
+syn_aggregate <- synMkdir(syn_release, "aggregate_data")
 
 c(
-  file.path(dir_release, "biochemicaldata_complete_inhouse_chembl.rds"),
-  file.path(dir_release, "biochemicaldata_complete_inhouse_chembl_Q1.rds"),
-  file.path(dir_release, "biochemicaldata_single_dose_inhouse.rds"),
-  file.path(dir_release, "biochemicaldata_single_dose_inhouse_Q1.rds"),
-  file.path(dir_release, "pheno_data.rds"),
-  file.path(dir_release, "pheno_data_Q1.rds")
+  file.path(dir_release, "biochemical_data_complete_q1.csv.gz"),
+  file.path(dir_release, "inhouse_single_dose_data_complete_q1.csv.gz"),
+  file.path(dir_release, "pheno_data_q1.csv.gz")
 ) %>%
   synStoreMany(parent = syn_aggregate, activity = aggregation_activity)
