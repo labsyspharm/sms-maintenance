03636701d50abab03501e1710876190d2ae31ccd
Clemens Hug <clemens.hug@gmail.com>
2021-08-27 11:26:11 -0400

    Improve load / save utility functions

--- utils/load_save.R
+++ utils/load_save.R
@@ -5,36 +5,41 @@ library(openxlsx)
 library(synapser)
 library(fst)
 
+.load_functions <- list(
+  `.csv` = fread,
+  `.tsv` = fread,
+  `.rds` = read_rds,
+  `.qs` = qread,
+  `.xlsx` = read.xlsx,
+  `.fst` = partial(read_fst, as.data.table = TRUE)
+)
+
+# Some columns are always incorrectly read as character by fread
+# https://github.com/Rdatatable/data.table/issues/4802
+.column_types <- list(
+  lspci_id = as.integer,
+  ontarget_IC50_Q1 = as.numeric,
+  offtarget_IC50_Q1 = as.numeric,
+  Q1 = as.numeric
+)
+
+.load_data <- function(x) {
+  message("Loading ", x)
+  read_fun <- .load_functions[[
+    which(str_detect(x, stringr::fixed(names(.load_functions))))
+  ]]
+  df <- read_fun(x)
+  cols <- intersect(colnames(df), names(.column_types)) %>%
+    set_names() %>%
+    map(function(col) .column_types[[col]](df[[col]]))
+  set(df, j = names(cols), value = unname(cols))
+  df
+}
 
 load_input_data <- function(inputs, syn) {
-  # Some columns are always incorrectly read as character by fread
-  # https://github.com/Rdatatable/data.table/issues/4802
-  column_types <- list(
-    lspci_id = as.integer,
-    ontarget_IC50_Q1 = as.numeric,
-    offtarget_IC50_Q1 = as.numeric,
-    Q1 = as.numeric
-  )
   inputs %>%
     map(syn) %>%
-    map(
-      function(x)
-        list(
-          `.csv` = fread,
-          `.tsv` = fread,
-          `.rds` = read_rds,
-          `.qs` = qread,
-          `.xlsx` = read.xlsx,
-          `.fst` = partial(read_fst, as.data.table = TRUE)
-        ) %>%
-        magrittr::extract2(which(str_detect(x, fixed(names(.))))) %>% {
-          df <- .(x)
-          cols <- intersect(colnames(df), names(column_types)) %>%
-            set_names() %>%
-            map(function(col) column_types[[col]](df[[col]]))
-          set(df, j = names(cols), value = unname(cols))
-        }
-    )
+    map(.load_data)
 }
 
 pluck_inputs <- function(inputs, syn_parent) {
